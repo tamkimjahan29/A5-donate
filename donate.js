3d97426795d51b93aa8be3890326fe98443aaf54
tamkimjahan29 <tamkimjahan@gmail.com>
2024-09-24 21:02:18 +0600

    adding functionality

--- donate.js
+++ donate.js
@@ -1,36 +1,44 @@
+document.getElementById('donate-now-btn').addEventListener('click',function(){
+    const balance=parseFloat(document.getElementById('balance').innerText);
+    const donationAmount = parseFloat(document.getElementById('donation-amount').value);
+    const donationAmountStudent = parseFloat(document.getElementById('donation-amount-student').innerText);
 
-// donation tab functionality
-
-const donationButton = document.getElementById('donation-tab')
-donationButton.addEventListener("click", function() {
-
-    const donationInput = document.getElementById('donation-amount')
-    console.log(donationInput) ;
+    const newBalance = balance - donationAmount ;
+    document.getElementById('balance').innerText = newBalance
     
-});
+    const newDonationAmount = donationAmountStudent + donationAmount;
+    document.getElementById('donation-amount-student').innerText =newDonationAmount;
+    
+})
+
+document.getElementById('button-donate').addEventListener('click',function(){
+
+const balance=parseFloat(document.getElementById('balance').innerText);
+const donationStudent= parseFloat(document.getElementById('donation-student').innerText);
+const amountDonate = parseFloat(document.getElementById('amount-donate').value);
+
+const newBalance = balance - amountDonate ;
+document.getElementById('balance').innerText = newBalance;
+
+const newAmountDonate = amountDonate + donationStudent ;
+document.getElementById('donation-student').innerText=newAmountDonate;
+
+
+})
+
+document.getElementById('donate').addEventListener('click',function(){
+
+    const balance=parseFloat(document.getElementById('balance').innerText);
+const donation= parseFloat(document.getElementById('donation').innerText);
+const amount = parseFloat(document.getElementById('amount').value);
+
+const newBalance = balance - amount ;
+document.getElementById('balance').innerText = newBalance;
+
+const newAmountDonate = amount+ donation ;
+document.getElementById('donation').innerText=newAmountDonate;
+
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// history tab functionality
-// const historyTab = document.getElementById('history-tab')
-// historyTab.addEventListener('click' , function() {
-
-//     historyTab.classList.add("bg-green-500", 
-//         "text-black ", 
-//         "font-semibold");
-
-// });
