03907c3159a32a88b45d18151bd8832f16fd926d
tamkimjahan29 <tamkimjahan@gmail.com>
2024-09-24 12:09:23 +0600

    added new section

---
+++ donate.js
@@ -0,0 +1,36 @@
+
+// donation tab functionality
+
+const donationButton = document.getElementById('donation-tab')
+donationButton.addEventListener("click", function() {
+
+    const donationInput = document.getElementById('donation-amount')
+    console.log(donationInput) ;
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// history tab functionality
+// const historyTab = document.getElementById('history-tab')
+// historyTab.addEventListener('click' , function() {
+
+//     historyTab.classList.add("bg-green-500", 
+//         "text-black ", 
+//         "font-semibold");
+
+// });
